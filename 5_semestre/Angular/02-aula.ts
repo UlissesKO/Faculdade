211cf995cb4707796cd68446f5925340ec7431b8
UlissesKO <ulisseskelermann@gmail.com>
2026-02-04 20:12:21 -0300

    Diretorios organizados

---
+++ 5_semestre/Angular/02-aula.ts
@@ -0,0 +1,15 @@
+var nome : string = "Ulisses" ;
+// < : string > -> Seta o tipo da variável
+
+var numero : number = 1234 ;
+// < : number > -> Numeros gerais (Inteiros ou float)
+
+console.log(nome, "_____", numero) ;
+
+//Para compilar o arquivo é tsc NomeDoArquivo no terminal
+
+var numBinario : number = 0b101010 ; //0b -> É um binário
+var numOctal : number = 0o76250 ; // 0o -> é um octal
+var numHex : number = 0xFA4 ; // 0x -> É um hexadecimal
+
+console.log(numBinario,"\n",numOctal,"\n",numHex) ;
