a8350c2bcafe6dde94bc0e37c9fe95652dc53e36
Ulisses <152618803+UlissesKO@users.noreply.github.com>
2026-02-03 21:48:11 -0300

    Add files via upload

---
+++ 5_semestre/XML,Ajax,Json/02-aula.js
@@ -0,0 +1,10 @@
+var nome = "Ulisses";
+// < : string > -> Seta o tipo da variável
+var numero = 1234;
+// < : number > -> Numeros gerais (Inteiros ou float)
+console.log(nome, "_____", numero);
+//Para compilar o arquivo é tsc NomeDoArquivo no terminal
+var numBinario = 42; //0b -> É um binário
+var numOctal = 31912; // 0o -> é um octal
+var numHex = 0xFA4; // 0x -> É um hexadecimal
+console.log(numBinario, "\n", numOctal, "\n", numHex);
